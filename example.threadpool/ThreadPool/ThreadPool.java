237953f4c08aa4a257bccfbdfce205318f856b13
xiang <xiang13225080@163.com>
2022-03-29 21:32:56 +0800

    minor update
    Signed-off-by: xiang <xiang13225080@163.com>

--- example.threadpool/ThreadPool/ThreadPool.java
+++ example.threadpool/ThreadPool/ThreadPool.java
@@ -9,11 +9,10 @@ import java.util.concurrent.Executors;
 
 public class ThreadPool {
     public static ThreadPoolExecutor pool;
-    //private static ExecutorService pool;
     public static void main( String[] args )
     {
         //maximumPoolSize设置为2 ，拒绝策略为AbortPolic策略，直接抛出异常
-        pool = new ThreadPoolExecutor(1, 1000, 1000, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(),Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+        pool = new ThreadPoolExecutor(1, 10, 10, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(),Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
         for(int i=0;i<100;i++) {
             try {
                 Thread.sleep(1000);
@@ -21,19 +20,9 @@ public class ThreadPool {
             } catch(Exception e) {
                 System.out.println("get exception when execute new thread:" + e);
             }
-        }   
-    }
-
-    public void attack() {
-        for(int i=0;i<1000;i++) {
-            try {
-                ThreadTask task = new ThreadTask();
-                task.setInterval(9999999);
-                pool.execute(task);
-            } catch(Exception e) {
-                System.out.println("get exception when execute new thread:" + e);
-                return;
-            }
         }
+
+        System.out.println("main thread: " + Thread.currentThread().getName());
+        return;
     }
 }
